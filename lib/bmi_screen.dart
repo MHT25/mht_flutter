6950e958dfbfa76846c484545373ac38f2ab066e
MHT25 <heshampeh@icloud.com>
2024-09-11 03:00:01 +0300

    Messenger screen

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -131,13 +131,92 @@ class _BmiScreenState extends State<BmiScreen> {
             )
           ),
           Expanded(
-            child: Container(
-              color:Colors.blue,
-              width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children:
+                [
+                  Expanded(
+                    child: Container(
+                      decoration:BoxDecoration(
+                      borderRadius:BorderRadius.circular(10),
+                      color: Colors.grey[400],
+                    ),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children:
+                        [
+                          Text('AGE',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),),
+                          Text('180',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 40,
+                            ),),
+                          Row(mainAxisAlignment: MainAxisAlignment.center,
+                            children:
+                            [
+                              FloatingActionButton(
+                                onPressed:(){},
+                                mini: true,
+                                child:Icon( Icons.remove),),
+                              FloatingActionButton(
+                                onPressed:(){},
+                                mini: true,
+                                child:Icon( Icons.add),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration:BoxDecoration(
+                        borderRadius:BorderRadius.circular(10),
+                        color: Colors.grey[400],
+                      ),
+                      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                        children:
+                        [
+                          Text('AGE',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),),
+                          Text('180',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 40,
+                            ),),
+                          Row(mainAxisAlignment: MainAxisAlignment.center,
+                            children:
+                            [
+                              FloatingActionButton(
+                                onPressed:(){},
+                                mini: true,
+                                child:Icon( Icons.remove),),
+                              FloatingActionButton(
+                                onPressed:(){},
+                                mini: true,
+                                child:Icon( Icons.add),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Container(width: double.infinity,
-            color: Colors.red,
+            color: Colors.blue,
             child: MaterialButton(onPressed: (){},
               height: 50,
             child: Text(
