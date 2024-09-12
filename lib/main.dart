7389ea8ea6129e9e2a225a9d1807b21175c6243d
MHT25 <heshampeh@icloud.com>
2024-09-12 11:01:11 +0300

    Messenger screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mht_flutter/bmi_result_screen.dart';
-import 'package:mht_flutter/counter_screen.dart';
-import 'package:mht_flutter/bmi_screen.dart';
-import 'package:mht_flutter/home_screen.dart';
-import 'package:mht_flutter/login_screen.dart';
-import 'package:mht_flutter/messenger_screen.dart';
-import 'package:mht_flutter/users_screen.dart';
+import 'package:mht_flutter/modules/bmi_result/bmi_result_screen.dart';
+import 'package:mht_flutter/modules/counter/counter_screen.dart';
+import 'package:mht_flutter/modules/bmi/bmi_screen.dart';
+import 'package:mht_flutter/modules/home/home_screen.dart';
+import 'package:mht_flutter/modules/login/login_screen.dart';
+import 'package:mht_flutter/modules/messenger/messenger_screen.dart';
+import 'package:mht_flutter/modules/users/users_screen.dart';
 
 void main() {
   runApp(MyApp());
